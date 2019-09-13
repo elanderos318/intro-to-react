6febac102a7ac29e54454b6d60937b4d3fe77783
elanderos <elanderos318@gmail.com>
2019-09-13 12:54:24 -0500

    created buttons which display past moves

--- src/index.js
+++ src/index.js
@@ -82,12 +82,24 @@ function Square(props) {
       const current = history[history.length - 1];
       const winner = calculateWinner(current.squares);
 
+      const moves = history.map((step, move) => {
+        const desc = move ?
+            'Go to move #' + move :
+            'Go to game start';
+        return (
+            <li>
+                <button onClick={() => this.jumpTo(move)}>{desc}</button>
+            </li>
+        );
+      });
+
       let status;
       if (winner) {
           status = 'Winner: ' + winner;
       } else {
           status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
       }
+
       return (
         <div className="game">
           <div className="game-board">
@@ -98,7 +110,7 @@ function Square(props) {
           </div>
           <div className="game-info">
             <div>{status}</div>
-            <ol>{/* TODO */}</ol>
+            <ol>{moves}</ol>
           </div>
         </div>
       );
